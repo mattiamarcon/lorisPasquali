57183259e1f451c81c134aeaf8c2a1ec87d6b101
mattiamarcon <mattiamarcon05@gmail.com>
2024-05-12 20:41:44 +0200

    aggiustamento particolari sito

--- src/pages/Contatti.jsx
+++ src/pages/Contatti.jsx
@@ -1,4 +1,7 @@
 import StandardPage from "../components/StandardPage";
+import { FaPhone } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 
 
@@ -6,40 +9,24 @@ export default function Contatti(){
     return (
         <>
             <StandardPage>
-            <h1 className="text-blue-700 text-5xl font-sans font-bold text-center m-5">CONTATTI</h1>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1961.1621482241155!2d12.528373817858844!3d45.8205092883968!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47795d5b95324513%3A0x11f5a394823538fc!2sBar%20Villeneuve!5e1!3m2!1sit!2sit!4v1713100611581!5m2!1sit!2sit" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="w-full h-[60vh] "></iframe>
+            <h1 className="text-center text-4xl font-bold my-5">CONTATTI</h1>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1961.1621482241155!2d12.528373817858844!3d45.8205092883968!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47795d5b95324513%3A0x11f5a394823538fc!2sBar%20Villeneuve!5e1!3m2!1sit!2sit!4v1713100611581!5m2!1sit!2sit" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="w-full h-[60vh]"></iframe>
             <div className="flex flex-col md:flex-row justify-center text-center my-7 text-wrap">
-            <div className="lg:w-1/4 flex flex-col md:h-56 justify-center my-5">
-                    <h2 className="font-bold text-3xl text-blue-700 mb-6">INDIRIZZO</h2>
-                    <a href="https://www.google.com/maps/dir//Bar+Villeneuve+Piazza+Europa,+10+31040+Mansu%C3%A8+TV/@45.8202778,12.53,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x47795d5b95324513:0x11f5a394823538fc!2m2!1d12.5300077!2d45.8203317?entry=ttu" className="text-xl underline underline-offset-2 hover:text-blue-500 cursor-pointer " target="_blank">VIA PER ODERZO,12-31040 Mansuè(TV)</a>
+                <div className="lg:w-1/4 flex flex-col md:h-56 justify-center my-5 mx-5 xl:mx-0">
+                    <FaMapMarkerAlt className="mb-6 md:w-1/3 sm:w-1/5 sm:h-1/12 w-1/3 md:h-1/3 h-1/3 mx-auto" />
+                    <a href="https://www.google.com/maps/dir//Bar+Villeneuve+Piazza+Europa,+10+31040+Mansu%C3%A8+TV/@45.8202778,12.53,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x47795d5b95324513:0x11f5a394823538fc!2m2!1d12.5300077!2d45.8203317?entry=ttu" className="text-2xl  hover:text-blue-500 cursor-pointer " target="_blank">Via per Oderzo-12, Mansuè(TV)</a>
                 </div>
-                <div className=" md:border-black md:border-l-4 lg:w-1/4 flex flex-col md:h-56 justify-center my-5">
-                    <h2 className="font-bold text-3xl text-blue-700 mb-6">TELEFONO</h2>
-                    <a href="tel:+393920142460" rel="noopener noreferrer" className="text-xl underline underline-offset-2 hover:text-blue-500 cursor-pointer">123 456 7890</a>
+                <div className="lg:w-1/4 flex flex-col md:h-56 justify-center my-5 mx-5 xl:mx-0">
+                    <FaPhone className="mb-6 md:w-1/3 sm:w-1/5 sm:h-1/12 w-1/3 md:h-1/3 h-1/3 mx-auto" />
+                    <a href="tel:+393920142460" rel="noopener noreferrer" className="text-2xl hover:text-blue-500 cursor-pointer">123 456 7890</a>
                 </div>
-                <div className=" md:border-black md:border-l-4 lg:w-1/4 flex flex-col md:h-56 justify-center my-5">
-                    <h2 className="font-bold text-3xl text-blue-700 mb-6">MAIL</h2>
-                    <a href="mailto:provamail@mail.com" rel="noopener noreferrer" className="text-xl underline underline-offset-2 hover:text-blue-500 cursor-pointer">provamail@mail.com</a>
+                <div className="lg:w-1/4 flex flex-col md:h-56 justify-center my-5 mx-5 xl:mx-0">
+                    <IoIosMail className="mb-6 md:w-1/3 sm:w-1/5 sm:h-1/12 w-1/3 md:h-1/3 h-1/3 mx-auto" />
+                    <a href="mailto:provamail@mail.com" rel="noopener noreferrer" className="text-2xl hover:text-blue-500 cursor-pointer">provamail@mail.com</a>
                 </div>
             </div>
             </StandardPage >
         </>
     );
 }
-
-/*
-     <div className="flex flex-col m-4">
-                <div className="flex flex-row w-full justify-center text-center p-2 ">
-                    <h2 className="font-bold text-xl whitespace-pre ">Indirizzo:  </h2>
-                    <a href="https://www.google.com/maps/dir//Bar+Villeneuve+Piazza+Europa,+10+31040+Mansu%C3%A8+TV/@45.8202778,12.53,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x47795d5b95324513:0x11f5a394823538fc!2m2!1d12.5300077!2d45.8203317?entry=ttu" className="text-xl underline underline-offset-2 hover:text-blue-500 cursor-pointer" target="_blank">VIA PER ODERZO,12-31040 Mansuè(TV)</a>
-                </div>
-                <div className="flex flex-row w-full justify-center text-center p-2 ">
-                    <h2 className="font-bold text-xl whitespace-pre">Telefono: </h2>
-                    <a href="tel:+393920142460" rel="noopener noreferrer" className="text-xl underline underline-offset-2 hover:text-blue-500 cursor-pointer">123 456 7890</a>
-                </div>
-                <div className="flex flex-row w-full justify-center text-center p-2 ">
-                    <h2 className="font-bold text-xl whitespace-pre">Email: </h2>
-                    <a href="mailto:provamail@mail.com" rel="noopener noreferrer" className="text-xl underline underline-offset-2 hover:text-blue-500 cursor-pointer">provamail@mail.com</a>
-                </div>
-    </div>
-*/
--- src/pages/Storia.jsx
+++ src/pages/Storia.jsx
@@ -5,14 +5,13 @@ import { Carousel } from "flowbite-react";
 
 export default function Storia(){
 
-  //sistemare bene graficamente pagina storia
 
     return (
       <>
         <StandardPage>
           <div className="grid place-items-center relative text-center">
-            <img src="famigliaConCasa.png" alt="" className="w-full opacity-70 h-72 lg:h-96 object-cover" />
-            <h1 className="absolute text-6xl font-bold text-red-700 underline underline-offset-2">I NOSTRI VALORI</h1>
+            <img src="famigliaConCasa.png" alt="" className="w-full opacity-70 h-24 object-cover" />
+            <h1 className="absolute text-4xl font-bold">I NOSTRI VALORI</h1>
           </div>
           <div className="flex flex-col xl:flex-row p-3 w-full justify-evenly mt-8">
             <div className="flex flex-col xl:w-1/4 m-3 text-center justify-evenly w-5/6 mx-auto">
@@ -21,13 +20,13 @@ export default function Storia(){
                 L'azienda edile "Loris Pasquali Immobiliare Costruzioni" è una nuova e dinamica realtà nel panorama edile, fondata e guidata con passione da un unico socio, il quale porta con sé l'antica tradizione familiare nel settore delle costruzioni. Cresciuto in una <strong>famiglia di muratori</strong> nel cuore della provincia di Treviso e del Veneto, il fondatore ha deciso di intraprendere questa avventura per rimanere vicino alla sua famiglia e per onorare la lunga eredità di maestria artigianale che lo ha preceduto.
               </p>
             </div>
-            <img src="foto2.png" alt="" className="xl:w-1/2 h-[260px] sm:h-[300px] md:h-[400px] lg:h-[450px] xl:h-[500px] w-11/12 mx-auto mb-3 "/>
+            <img src="foto2.png" alt="" className="xl:w-1/2 h-[260px] sm:h-[300px] md:h-[400px] lg:h-[450px] xl:h-[500px] w-11/12 md:w-9/12 mx-auto mb-3 "/>
           </div>
           <hr />
           <div className="flex flex-col sm:flex-row p-3 w-full justify-evenly flex-wrap ">
-          <div className="flex flex-col sm:w-2/5 xl:w-1/4 m-3 text-center justify-evenly w-5/6 mx-auto">
+            <div className="flex flex-col sm:w-2/5 xl:w-1/4 m-3 text-center justify-evenly w-5/6 mx-auto">
               <h1 className=" font-semibold text-4xl mb-5 2xl:mb-0">LA TRADIZIONE</h1>
-              <p className="mb-8">
+              <p className="">
                 Guidato dalla convinzione che il vero valore di un'impresa risieda nella sua capacità di mettere al centro le esigenze delle persone e di creare spazi che ispirino e trasformino, il fondatore si dedica con fervore alle ristrutturazioni e alle costruzioni. Con una visione orientata al futuro e un profondo rispetto per le radici, "Loris Pasquali Immobiliare Costruzioni" si impegna a offrire soluzioni innovative e su misura per ogni progetto, mantenendo sempre vivo lo spirito dell'artigianato tradizionale.
               </p>
             </div>
@@ -37,7 +36,7 @@ export default function Storia(){
             
             <div className="flex flex-col lg:w-2/5 xl:w-1/4 m-3 text-center justify-evenly w-5/6 mx-auto">
               <h1 className=" font-semibold text-4xl mb-5 2xl:mb-0">IL NOSTRO TERRITORIO</h1>
-              <p className="mb-8">
+              <p className="">
                 Nato nel cuore della provincia di Treviso e del Veneto, l'azienda ha rapidamente guadagnato fiducia e riconoscimento nella comunità locale, grazie alla sua reputazione di affidabilità, professionalità e attenzione ai dettagli. Attraverso un costante impegno per l'eccellenza e un approccio centrato sul cliente, "Loris Pasquali Immobiliare Costruzioni" si propone di diventare un punto di riferimento nel settore edile, trasformando le aspirazioni dei clienti in realtà tangibili e durature.
               </p>
             </div>
